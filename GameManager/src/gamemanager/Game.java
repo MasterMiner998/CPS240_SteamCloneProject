bbdec7c24ba5e7d6e8fcbe609ab3c69d4f8b9296
MasterMiner998 <mastermining998@gmail.com>
2025-11-20 15:42:02 -0500

    Updated game.java with toString and others

--- GameManager/src/gamemanager/Game.java
+++ GameManager/src/gamemanager/Game.java
@@ -3,16 +3,24 @@ package gamemanager;
 import java.awt.Image;
 
 public class Game implements Comparable<Game> {
+	public static SortType sortType = SortType.Alphabetical;
+	public static enum SortType{
+		Alphabetical,
+		Rating,
+		Playtime,
+		ReleaseDate
+	}
 	
-	private String Name;
-	private String Developer;
-	private String Publisher;
-	private String[] Genres;
+	private String name;
+	private String developer;
+	private String publisher;
+	private String[] genres;
 	
-	private Double Rating;
-	private Double PlayTime;
+	private Double rating;
+	private Double playTime;
+	private String releaseDate;
 	
-	private Image Cover;
+	private Image cover;
 
 	@Override
 	public int compareTo(Game game) {
@@ -20,4 +28,9 @@ public class Game implements Comparable<Game> {
 		return 0;
 	}
 
+	@Override
+	public String toString() {
+		return String.format("%s, by %s, published by: %s", name, developer, publisher);
+	}
+	
 }
