ecdba1cc75c2372d39eb2902b59d46da487bbbfa
MasterMiner998 <mastermining998@gmail.com>
2025-11-25 15:16:00 -0500

    Added functions to Account. Should be ready for use

--- GameManager/src/gamemanager/Account.java
+++ GameManager/src/gamemanager/Account.java
@@ -4,24 +4,42 @@ import java.util.ArrayList;
 
 public class Account {
 
-	private String UserName;
+	private String userName;
 	private GameCollection library;
 	private ArrayList<GameCollection> collections;
 	
 	public Account(String userName) {
-		this.UserName = userName;
+		this.userName = userName;
 		this.library = new GameCollection("Library");
 		this.collections = new ArrayList<GameCollection>();
 	}
 	public Account(String userName, ArrayList<Game> gameList) {
-		this.UserName = userName;
+		this.userName = userName;
 		this.library = new GameCollection("Library", gameList);
 		this.collections = new ArrayList<GameCollection>();
 	}
-	
+	public String getUserName() {
+		return this.userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 	public void addToLibrary(Game game) {
 		this.library.addGame(game);
 	}
-	
-	
+	public void removeFromLibrary(Game game) {
+		this.library.removeGame(game);
+	}
+	public void addCollection(String name) {
+		collections.add(new GameCollection(name));
+	}
+	public void removeCollection(int i) {
+		collections.remove(i);
+	}
+	public void addToCollection(int i, Game game) {
+		collections.get(i).addGame(game);
+	}
+	public void removeFromCollection(int i, Game game) {
+		collections.get(i).removeGame(game);
+	}
 }
--- GameManager/src/gamemanager/GameCollection.java
+++ GameManager/src/gamemanager/GameCollection.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 public class GameCollection {
 	String name;
 	ArrayList<Game> gcSorted;
-	ArrayList<Game> gameCollectionDisplay;
+	//ArrayList<Game> gameCollectionDisplay;
 	
 	public GameCollection(String name) {
 		super();
@@ -28,9 +28,9 @@ public class GameCollection {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public ArrayList<Game> getGameCollection() {
-		return gameCollectionDisplay;
-	}
+//	public ArrayList<Game> getGameCollection() {
+//		return gameCollectionDisplay;
+//	}
 	public void setGameCollection(ArrayList<Game> gameCollection) {
 		this.gcSorted = gameCollection;
 	}
@@ -41,7 +41,37 @@ public class GameCollection {
 		//Game.sortType = Game.SortType.Alphabetical;
 		this.gcSorted.sort(null);
 	}
-	public Game getGame(String x) {
+	public void removeGame(Game game) {
+		int a = this.containsGame(game);
+		if(a < 0) {
+			System.out.printf("%s does not contain %s", this.name, game.getName());
+			return;
+		}
+		gcSorted.remove(a);
+		//Remove from display list
+	}
+	public int containsGame(Game game) {
+		int l = 0;
+        int r = gcSorted.size() - 1;
+
+        while (l <= r) {
+
+            int m = l + (r - l) / 2;
+
+            if (gcSorted.get(m).equals(game)) {
+                return m;
+            }
+            if (gcSorted.get(m).compareTo(game) < 0) {
+                l = m + 1;
+            }
+            else {
+                r = m - 1;
+            }
+        }
+		
+		return -1;
+	}
+	public Game fidGame(String x) {
 		int l = 0;
         int r = gcSorted.size() - 1;
 
