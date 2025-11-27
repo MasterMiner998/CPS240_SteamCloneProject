f7edd8263758f373e7294452205dc74cd97df491
MasterMiner998 <mastermining998@gmail.com>
2025-11-27 15:52:01 -0500

    I think I added JavaFX. Others might need to add it themselves

--- GameManager/src/gamemanager/GameManager.java
+++ GameManager/src/gamemanager/GameManager.java
@@ -1,11 +1,22 @@
 package gamemanager;
 
-public class GameManager {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GameManager extends Application{
 	
-	
+	@Override
+	public void start(Stage stage) throws Exception {
+		GridPane gPane = new GridPane();
+		Scene scene = new Scene(gPane, 1000, 800);
+		stage.setTitle("Game Manager");
+		stage.setScene(scene);
+		stage.show();
+	}
 	
 	public static void main(String[] args) {
-				
+		launch(args);
 	}
-
 }
--- GameManager/src/module-info.java
+++ GameManager/src/module-info.java
@@ -6,4 +6,5 @@
  */
 module GameManager {
 	requires java.desktop;
+	requires javafx.graphics;
 }
