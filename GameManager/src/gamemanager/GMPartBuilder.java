602c354b4a65af0025fb18ca5abd0d7bda6b518d
MasterMiner998 <mastermining998@gmail.com>
2025-12-01 02:35:29 -0500

    Changed styling on game pages

--- GameManager/src/gamemanager/GMPartBuilder.java
+++ GameManager/src/gamemanager/GMPartBuilder.java
@@ -2,6 +2,8 @@ package gamemanager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -17,26 +19,28 @@ public class GMPartBuilder {
 		box.setStyle(String.format("-fx-background-color: %s", GameManager.STEAM_MAIN_COLOR));
 		try {
 			Label name = new Label(game.getName());
-			name.setStyle("-fx-text-fill: white;");
+			name.setStyle("-fx-text-fill: white; -fx-font-size: 20; -fx-font-weight: bold;");
+			name.setPadding(new Insets(0,0,5,0));
 			box.getChildren().add(name);
 		} catch(NullPointerException e) {}
 		
 		try {
-			Label dev = new Label(game.getDeveloper());
+			Label dev = new Label("Developer: " + game.getDeveloper());
 			dev.setStyle("-fx-text-fill: white;");
 			box.getChildren().add(dev);
 		}catch(NullPointerException e) {}
 		
 		try {
-			Label pub = new Label(game.getPublisher());
+			Label pub = new Label("Publisher: " + game.getPublisher());
 			pub.setStyle("-fx-text-fill: white;");
+			pub.setPadding(new Insets(0,0,10,0));
 			box.getChildren().add(pub);
 		} catch(NullPointerException e) {}
 		
 		try {
-			String genreString = "";
+			String genreString = "Genres: ";
 			for(String g : game.getGenres()) {
-				genreString += g + ", ";
+				genreString += g + ",";
 			}
 			Label genres = new Label(genreString);
 			genres.setStyle("-fx-text-fill: white;");
@@ -44,19 +48,19 @@ public class GMPartBuilder {
 		} catch(NullPointerException e) {}
 		
 		try {
-			Label rating = new Label(game.getRating().toString());
+			Label rating = new Label("Rating: " + game.getRating().toString());
 			rating.setStyle("-fx-text-fill: white;");
 			box.getChildren().add(rating);
 		} catch(NullPointerException e) {}
 		
 		try {
-			Label playTime = new Label(game.getPlayTime().toString());
+			Label playTime = new Label("Play time: " + game.getPlayTime().toString());
 			playTime.setStyle("-fx-text-fill: white;");
 			box.getChildren().add(playTime);
 		}catch(NullPointerException e) {}
 		
 		try {
-			Label release = new Label(game.getReleaseDate());
+			Label release = new Label("Release Date: " + game.getReleaseDate());
 			release.setStyle("-fx-text-fill: white;");
 			box.getChildren().add(release);
 		} catch(NullPointerException e) {}
